773b000708e030a171166c9797b29568f2855dda
David Silvestri <dsilvestri@Mac.fibertel.com.ar>
2025-06-22 20:27:25 -0300

    prompt to get response from gemini

--- src/gemini/gemini.service.ts
+++ src/gemini/gemini.service.ts
@@ -1,10 +1,19 @@
 import { Injectable } from '@nestjs/common';
 import { BasicPromptDto } from './dtos/basic-prompt.dto';
+import { GoogleGenAI } from '@google/genai';
 
 @Injectable()
 export class GeminiService {
-
-    basicPrompt(basicPromptDto: BasicPromptDto){
-       return basicPromptDto
+    private ai = new GoogleGenAI({apiKey: process.env.GEMINI_API_KEY});
+    
+    async basicPrompt(basicPromptDto: BasicPromptDto){
+        const response = await this.ai.models.generateContent({
+            model: "gemini-2.5-flash",
+            contents: basicPromptDto.prompt,
+            config:{
+                systemInstruction: 'Responds in english, markdown format'
+            }
+        });
+       return response.text;
     }
 }
